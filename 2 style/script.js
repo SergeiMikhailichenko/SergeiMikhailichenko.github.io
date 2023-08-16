712d41d871cdf143de739fd0b5df88bc8b00fbbf
Sergei Mikhailichenko <mss230888@gmail.com>
2023-08-16 21:41:05 +0300

    fix 2 style

--- 2 style/script.js
+++ 2 style/script.js
@@ -1,8 +1,11 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
+const mainPage = document.querySelector('.main');
+
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('active');
+    mainPage.classList.toggle('deactive');
 });
 
 window.addEventListener('resize', () => {
