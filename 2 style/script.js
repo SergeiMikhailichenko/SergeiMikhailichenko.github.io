8d7116a7044ba023add707f565d753e62e0a3e76
Sergei Mikhailichenko <mss230888@gmail.com>
2023-08-17 16:04:21 +0300

    add new page python

--- 2 style/script.js
+++ 2 style/script.js
@@ -1,7 +1,7 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 const mainPage = document.querySelector('.main');
-
+const pythonPage = document.querySelector('.lang-in-list.python');
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('active');
@@ -12,4 +12,8 @@ window.addEventListener('resize', () => {
     if (window.innerWidth > 768) {
         nav.classList.remove('active');
     }
+});
+
+pythonPage.addEventListener('click', () => {
+    window.location.href = 'python.html';
 });
