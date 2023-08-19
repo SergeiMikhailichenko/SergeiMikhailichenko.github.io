edd5c870d89a9b262de096da0cfdae71f86ecb27
Sergei Mikhailichenko <mss230888@gmail.com>
2023-08-19 22:11:06 +0300

    add buttons for JS and HTML

--- 2 style/script.js
+++ 2 style/script.js
@@ -2,6 +2,8 @@ const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 const mainPage = document.querySelector('.main');
 const pythonPage = document.querySelector('.lang-in-list.python');
+const javascriptPage = document.querySelector('.lang-in-list.javascript');
+const htmlPage = document.querySelector('.lang-in-list.html');
 const btnStartPython = document.querySelector('.btn-start.python');
 
 burger.addEventListener('click', () => {
@@ -14,7 +16,7 @@ window.addEventListener('resize', () => {
         nav.classList.remove('active');
     }
 });
-
+// Python
 pythonPage?.addEventListener('click', () => {
     window.location.href = 'python.html';
 });
@@ -22,3 +24,15 @@ pythonPage?.addEventListener('click', () => {
 btnStartPython?.addEventListener('click', () => {
     window.location.href = 'python/lesson1.html';
 });
+// end Python
+
+// JavaScript
+javascriptPage?.addEventListener('click', () => {
+    window.location.href = 'javascript.html'
+});
+
+// HTML
+htmlPage?.addEventListener('click', () => {
+    window.location.href = 'html5.html'
+});
+// end HTML
